129c2d08e8f8e9b58fdebd3735a29f7b745bb88e
tthote <tthote@iu.edu>
2020-10-25 16:24:45 -0400

    changes bs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 
